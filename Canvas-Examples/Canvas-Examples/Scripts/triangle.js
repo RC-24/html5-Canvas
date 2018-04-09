5958765776224b05239263b25ef978c0f4c8ffa4
RC-24 <cullotyross@eircom.net>
2018-04-09 18:55:40 +0100

    Create script to draw a triangle shape

---
+++ Canvas-Examples/Canvas-Examples/Scripts/triangle.js
@@ -0,0 +1,16 @@
+﻿let canvas = document.getElementById("example");
+let renderingContext = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+DrawTriangle();
+
+function DrawTriangle() {
+    renderingContext.beginPath();
+    renderingContext.moveTo(canvas.width * 0.5, canvas.height * 0.1);
+    renderingContext.lineTo(canvas.width * 0.75, canvas.height * 0.9);
+    renderingContext.lineTo(canvas.width * 0.25, canvas.height * 0.9);
+    renderingContext.closePath();
+    renderingContext.stroke();
+}
