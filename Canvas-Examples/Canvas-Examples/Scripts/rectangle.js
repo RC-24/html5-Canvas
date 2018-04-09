3ec5eaceb42c668b4bb75fcd8fa3cfef3ff1852e
RC-24 <cullotyross@eircom.net>
2018-04-09 21:07:41 +0100

    Added simple rectangle script

---
+++ Canvas-Examples/Canvas-Examples/Scripts/rectangle.js
@@ -0,0 +1,19 @@
+﻿let canvas = document.getElementById("example");
+let renderingContext = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+drawRectangles();
+
+function drawRectangles() {
+    // Rectangle 1
+    renderingContext.fillRect(90, 90, 150, 70);
+
+    // Rectangle 2
+    renderingContext.strokeRect(400, 90, 250, 400);
+
+    // Rectangle 3
+    renderingContext.fillRect(800, 90, 200, 90);
+    renderingContext.clearRect(810, 100, 180, 70);
+}
