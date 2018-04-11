0262580109a33eff090bf6e18131e85e49dfd1b1
RC-24 <cullotyross@eircom.net>
2018-04-11 19:50:04 +0100

    Updated randomCircles.js to draw each circle with a random color

--- Canvas-Examples/Canvas-Examples/Scripts/randomCircles.js
+++ Canvas-Examples/Canvas-Examples/Scripts/randomCircles.js
@@ -4,7 +4,7 @@ let renderingContext = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-drawRandomCircles(20);
+drawRandomCircles(200);
 
 function drawRandomCircles(numberOfCircles) {
     let maxCircleSize = 100;
@@ -13,9 +13,19 @@ function drawRandomCircles(numberOfCircles) {
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height;
         let radius = Math.random() * maxCircleSize;
+        let color = getRandomRgbColor();
 
         renderingContext.beginPath();
         renderingContext.arc(x, y, radius, 0, Math.PI * 2, false);
+        renderingContext.strokeStyle = color;
         renderingContext.stroke();
     }
+}
+
+function getRandomRgbColor() {
+    let red = Math.floor(Math.random() * 255.99);
+    let green = Math.floor(Math.random() * 255.99);
+    let blue = Math.floor(Math.random() * 255.99);
+
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
