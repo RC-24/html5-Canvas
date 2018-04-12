5c7719a643e06d26d609e2dbbd3099254c5ad103
RC-24 <cullotyross@eircom.net>
2018-04-12 21:04:56 +0100

    Practicing using the quadraticCurveTo() method

---
+++ Canvas-Examples/Canvas-Examples/Scripts/quadraticCurvePractice.js
@@ -0,0 +1,18 @@
+﻿let canvas = document.getElementById("example");
+let renderingContext = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+drawQuadraticCurve();
+
+function drawQuadraticCurve() {
+    let startX = 500;
+    let startY = 500;
+    let endX = 450;
+    let endY = 600;
+
+    renderingContext.moveTo(startX, startY);
+    renderingContext.quadraticCurveTo(300, 300, endX, endY)
+    renderingContext.stroke();
+}
